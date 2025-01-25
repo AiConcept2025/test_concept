87ccec01fed2935943a84d39638d6f7e11a424dc
Michael Kaplan <mkaplanmnj@yahoo.com>
2025-01-24 23:05:15 -0500

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,14 @@
-import { initializeDriveClient, uploadToGoogleDrive, fetchStartPageToken, fetchChanges } from './utils';
+import { initializeDriveClient, uploadToGoogleDrive, fetchStartPageToken, fetchChanges } from './utils.js';
 
-const world = 'world';
-
-export function hello(who: string = world): string {
-    return `Hello ${who}! `;
-}
 
 const localFilePath = 'data/ipsum.txt';
 const googleDriveFolderId = '1R4g1cSZUZ5nC2bzo7RxRO_46so5uYJS8';
 
-console.log(hello());
-
-// Promise.resolve()
-//     .then(() => initializeDriveClient('./secret/service-account-key.json'))
-//     .then(drive => uploadToGoogleDrive(drive, localFilePath, googleDriveFolderId))
-//     .then(res => { console.log(res); })
-//     .catch((error) => { console.log(error.message); });
-
-// Promise.resolve()
-//     .then(() => initializeDriveClient('./secret/service-account-key.json'))
-//     .then(drive => fetchStartPageToken(drive))
-//     .then(res => { console.log(res); })
-//     .catch((error) => { console.log(error.message); });
-
-Promise.resolve()
-    .then(() => initializeDriveClient('./secret/service-account-key.json'))
-    .then(drive => fetchChanges(drive, '64'))
-    .then(res => { console.log(res); })
-    .catch((error) => { console.log(error.message); });
+try {
+    const drive = await initializeDriveClient('./secret/service-account-key.json');
+    const res = await uploadToGoogleDrive(drive, localFilePath, googleDriveFolderId);
+    console.log(res);
+}
+catch (e) {
+    console.log((e as Error).message);
+}
